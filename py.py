d56438fc95ffd4c30d320ef84d28b37a4f1a86c9
Cpriyanshi07 <95691120+Cpriyanshi07@users.noreply.github.com>
2025-07-11 00:09:57 +0530

    Create py.py

---
+++ py.py
@@ -0,0 +1,8 @@
+# hello_python_script.py
+
+def main():
+    print("Hello! This is your first Python script.")
+    print("Welcome to the world of Python programming!")
+
+if __name__ == "__main__":
+    main()
